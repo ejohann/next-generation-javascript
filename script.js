39b48d017d9e1c0b7047315e8223fdaa9564325f
Johanne <johanne_lewis@hotmail.com>
2019-08-04 13:11:45 +0100

    Only have access to this keyword in object

--- script.js
+++ script.js
@@ -130,4 +130,32 @@ age6 = years.map((el, index) => {
 console.log(age6);
 
 
+/***********************************
+***  Arrow functions 2
+**********************************/
+
+
+// ES5 
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        document.querySelector('.green').addEventListener('click', function(){
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+box5.clickMe();
+
+
+
+
+
+
+
+
+
+
+
 
