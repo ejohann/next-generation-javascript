99844d1e5659654906c2fb0563758f8d9ae749b3
Johanne <johanne_lewis@hotmail.com>
2019-08-05 08:56:33 +0100

    ES5 - break and continue

--- script.js
+++ script.js
@@ -255,19 +255,26 @@ console.log(retirement);
 const boxes = document.querySelectorAll('.box');
 
 //ES5
-/*
+
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur){
    cur.style.backgroundColor = 'dodgerblue'; 
 });
-*/
 
-//ES6 
+
+//ES6
+/*
 const boxesArr6 = Array.from(boxes);
 boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+*/
 
-
-
+//ES5 
+for(var i = 0; i < boxesArr5.length; i++){
+    if(boxesArr5[i].className === 'box blue'){
+        break;
+    }
+    boxesArr5[i].textContent = 'I changed to blue';
+}
 
 
 
