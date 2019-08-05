79b16de02463237590f07b7511a8dcd63659bc29
Johanne <johanne_lewis@hotmail.com>
2019-08-05 11:10:56 +0100

    ES6 - maps

--- script.js
+++ script.js
@@ -420,7 +420,7 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
 console.log(emily);
 */
 
-
+/*
 //ES6 
 function SmithPerson(firstName,yearOfBirth, lastName = 'Smith', nationality = 'british'){
     this.firstName = firstName;
@@ -434,3 +434,51 @@ console.log(john);
 
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
 console.log(emily);
+*/
+
+
+/***********************************
+***  Maps
+**********************************/
+
+const question = new Map();
+question.set('question', 'What is the official name of the latest major javascript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer');
+question.set(false, 'Wrong, please try again');
+
+console.log(question);
+console.log(question.get('question'));
+console.log(question.size);
+
+if(question.has(4)){
+    question.delete(4);
+}
+
+console.log(question);
+
+question.clear();
+console.log(question);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
